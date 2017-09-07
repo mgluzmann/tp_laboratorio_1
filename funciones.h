8fe57f3d47a6015919780c8a5389ae0ccda12799
mgluzmann <manuel.gluzmann@gmail.com>
2017-09-07 16:44:57 -0300

    Trabajo practio 1 para entrega. Manuel Gluzmann.

--- funciones.h
+++ funciones.h
@@ -1,113 +1,14 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-
-
-
-
 float sumar(float, float);
-/** \brief Suma 2 numeros previamente ingresados.
- *
- * \param primer numero ingresado.
- * \param segundo numero ingresado.
- * \return la suma de ambos numeros.
- *
- */
 
 float restar(float, float);
-/** \brief Resta 2 numeros previamente ingresados.
- *
- * \param primer numero ingresado.
- * \param segundo numero ingresado.
- * \return la resta del primer numero menos el segundo numero.
- *
- */
 
 float multiplicar(float, float);
-/** \brief Multiplica 2 numeros previamente ingresados.
- *
- * \param primer numero ingresado.
- * \param segundo numero ingresado.
- * \return el producto proveniente de la multiplicacion de ambos numeros.
- *
- */
 
 float dividir(float, float);
-/** \brief Divide 2 numeros previamente ingresados.
- *
- * \param primer numero ingresado, dividendo.
- * \param  segundo numero ingresado, divisor
- * \return el resultado de la division entre el dividendo y el divisor.
- *
- */
 
 float factorial(float);
-/** \brief funcion recursiva para calcular factoriales
- *
- * \param el primero numero ingresado.
- * \return 1 cuando el numero es 0 o vuelve a llamarse a si misma hasta alcanzar 1!.
- *
- */
-
-
-float sumar(float x, float y)
-{
-    float suma;
-
-   suma = x + y;
-
-    return suma;
-}
-
-float restar(float x, float y)
-{
-    float resta;
-
-   resta = x - y;
-
-    return resta;
-}
-
-float multiplicar(float x, float y)
-{
-    float producto;
-
-    producto = x * y;
-
-    return producto;
-}
-
-float dividir(float x, float y)
-{
-    float division;
-
-    division = x / y;
-
-    return division;
-}
-
-float factorial (float x)
-{
-    if (x<=1)
-    {
-        return 1;
-    }
-    else if(x>1)
-    {
-        return x*factorial(x-1);
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-
-
-
-
-
-
-
 
 #endif // FUNCIONES_H_INCLUDED
--- main.c
+++ main.c
@@ -4,67 +4,138 @@
 
 int main()
 {
-    char seguir;
+    char seguir = 's';
     int opcion=0;
     float x;
     float y;
+    int flag1=0;
+    int flag2=0;
 
-    do
+    while(seguir == 's')
     {
-        printf("1- Ingresar 1er operando (A=%f)\n",x);
-        printf("2- Ingresar 2do operando (B=%f)\n",y);
-        printf("3- Calcular la suma (%f+%f)\n",x,y);
-        printf("4- Calcular la resta (%f-%f)\n",x,y);
-        printf("5- Calcular la division (%f/%f)\n",x,y);
-        printf("6- Calcular la multiplicacion (%f*%f)\n",x,y);
-        printf("7- Calcular el factorial (%.f!)\n",x);
+        system("cls");
+
+        if(flag1 == 1)
+        {
+            printf("1- Ingresar 1er operando (A=%.2f)\n",x);
+        }
+        else
+        {
+            printf("1- Ingresar 1er operando (A=x)\n");
+        }
+
+        if(flag2 == 1)
+        {
+            printf("2- Ingresar 2do operando (B=%.2f)\n",y);
+        }
+        else
+        {
+            printf("2- Ingresar 2do operando (B=y)\n");
+        }
+
+        printf("3- Calcular la suma (A+B)\n");
+        printf("4- Calcular la resta (A-B)\n");
+        printf("5- Calcular la division (A/B)\n");
+        printf("6- Calcular la multiplicacion (A*B)\n");
+        printf("7- Calcular el factorial (A!)\n");
         printf("8- Calcular todas las operaciones\n");
         printf("9- Salir\n");
 
         scanf("%d",&opcion);
+        system("cls");
+
 
         switch(opcion)
         {
             case 1:
-                printf("Ingresar 1er operando: \n");
+                printf("Ingresar 1er operando: ");
                 scanf("%f",&x);
+                flag1 = 1;
                 break;
             case 2:
-                printf("Ingresar 2do operando: \n");
+                printf("Ingresar 2do operando: ");
                 scanf("%f",&y);
+                flag2 = 1;
                 break;
             case 3:
-                printf("La suma de los dos operandos es igual a: %f\n",sumar(x,y));
+                if(flag1 == 0 || flag2 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1 y/o 2\n\n");
+                }
+                else
+                {
+                    printf("La suma de los dos operandos es igual a: %f\n",sumar(x,y));
+                }
                 break;
             case 4:
-                printf("La resta de los dos operandos es igual a: %f\n",restar(x,y));
+                if(flag1 == 0 || flag2 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1 y/o 2");
+                }
+                else
+                {
+                    printf("La resta de los dos operandos es igual a: %f\n",restar(x,y));
+                }
+
                 break;
             case 5:
+                 if(flag1 == 0 || flag2 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1 y/o 2\n\n");
+                }
+
+                else
+                {
                 while(y==0)
                     {
                         printf("No se puede dividir por cero, por favor ingrese otro divisor:\n");
                         scanf("%f",&y);
                     }
                 printf("La division de los dos operandos es igual a: %f\n",dividir(x,y));
+                }
                 break;
             case 6:
-                printf("El producto de los dos operandos es igual a: %f\n",multiplicar(x,y));
+                if(flag1 == 0 || flag2 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1 y/o 2\n\n");
+                }
+                else
+
+                {
+                    printf("El producto de los dos operandos es igual a: %f\n",multiplicar(x,y));
+                }
                 break;
             case 7:
-                printf("El factorial del operando A es igual a: %.f\n",factorial(x));
+                if(flag1 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1\n\n");
+                }
+
+                else
+                {
+                    printf("El factorial del operando A es igual a: %.2f\n",factorial(x));
+                }
                 break;
             case 8:
-                printf("La suma de los dos operandos es igual a: %f\n",sumar(x,y));
-                printf("La resta de los dos operandos es igual a: %f\n",restar(x,y));
-                printf("El producto de los dos operandos es igual a: %f\n",multiplicar(x,y));
-                printf("El factorial del operando A es igual a: %.f\n",factorial(x));
+                if(flag1 == 0 || flag2 == 0)
+                {
+                    printf("Error. Para iniciar elija opcion 1 y/o 2\n\n");
+                }
+                else
+                {
+                    printf("La suma de los dos operandos es igual a: %.2f\n", sumar(x,y));
+                    printf("La resta de los dos operandos es igual a: %.2f\n", restar(x,y));
+                    printf("El producto de los dos operandos es igual a: %.2f\n", multiplicar(x,y));
+                    printf("El factorial del operando A es igual a: %.2f\n", factorial(x));
 
-                while(y==0)
+                    while(y==0)
                     {
                         printf("No se puede dividir por cero, por favor ingrese otro divisor:\n");
                         scanf("%f",&y);
                     }
                 printf("La division de los dos operandos es igual a: %f\n",dividir(x,y));
+                }
+
                 break;
             case 9:
                 seguir = 'n';
@@ -72,20 +143,29 @@ int main()
 
         }
 
-        printf("Desea continuar? <s/n>");
-        fflush(stdin);
-        scanf("%c",&seguir);
+        if (seguir == 's')
+        {
+            printf("\n\nDesea continuar? <s/n>");
+            fflush(stdin);
+            scanf("%c",&seguir);
+        }
 
-        }while(seguir=='s');
+
+}
+
+        system("cls");
+
+        printf("\n\nGracias por usar esta calculadora!\n\n");
+
+
 
 
 
 
     return 0;
 
+    }
 
 
-
-}
 
 
